5cedce2a8a8df71b8a555bd6f4c940a19ed989e7
piotrszlejter <piotr.szlejter@gmail.com>
2026-07-14 17:47:09 +0200

    fix: runs correct level

--- game/stages/main_menu.gd
+++ game/stages/main_menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://stages/Level1.tscn")
+	get_tree().change_scene_to_file("res://stages/level1_v2.tscn")
 
 
 func _on_exit_button_pressed() -> void:
